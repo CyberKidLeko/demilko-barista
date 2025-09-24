d01226dd5ff153a5e4b0a26b2c3f1d7e855814ea
CyberKidLeko <nkululekongwenya123@gmail.com>
2025-09-25 01:41:04 +0200

    Fix global border issue

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // all your JS/TS/React files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
